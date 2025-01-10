99bc47fae28a4b2160dacdf17c5b027d0314348f
turgaymammadov <memmedovturqay@871gmail.com>
2025-01-10 21:22:51 +0400

    s

--- frontend/src/Socket.js
+++ frontend/src/Socket.js
@@ -1,3 +1,3 @@
 import {io} from "socket.io-client";
-const URL=process.env.NODE_ENV==='production' ? 'https://realchatapp-virid.vercel.app/' : "http://localhost:5000";
+const URL=process.env.NODE_ENV==='production' ? 'https://realchatapp-virid.vercel.app' : "http://localhost:5000";
 export const socket=io(URL)
