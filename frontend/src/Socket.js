bf3527d8c755d85f5360b278571b37e5f4006a35
turgaymammadov <memmedovturqay@871gmail.com>
2025-01-10 20:01:14 +0400

    s

--- frontend/src/Socket.js
+++ frontend/src/Socket.js
@@ -1,3 +1,3 @@
 import {io} from "socket.io-client";
-const URL=process.env.NODE_ENV==='production' ? undefined : "http://localhost:5000";
+const URL=process.env.NODE_ENV==='production' ? 'https://chatapp-mppodtnqj-turqay667s-projects.vercel.app' : "http://localhost:5000";
 export const socket=io(URL)
