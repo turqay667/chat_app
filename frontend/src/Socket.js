5a66c66ac9d6c51ee16cb8ce9d24a38e0bfff69b
turgaymammadov <memmedovturqay@871gmail.com>
2025-01-10 21:05:26 +0400

    sss

--- frontend/src/Socket.js
+++ frontend/src/Socket.js
@@ -1,3 +1,3 @@
 import {io} from "socket.io-client";
-const URL=process.env.NODE_ENV==='production' ? 'https://chat-app-bxnf-17xxl1swm-turqay667s-projects.vercel.app/' : "http://localhost:5000";
+const URL=process.env.NODE_ENV==='production' ? 'ttps://chat-app-bxnf.vercel.app' : "http://localhost:5000";
 export const socket=io(URL)
