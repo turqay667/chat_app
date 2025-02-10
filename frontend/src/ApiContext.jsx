7f37c0543db903b298d384498474f401faaa8869
turgaymammadov <memmedovturqay871@gmail.com>
2025-02-10 15:08:16 +0400

    e

--- frontend/src/ApiContext.jsx
+++ frontend/src/ApiContext.jsx
@@ -1,7 +1,7 @@
 import { createContext } from "react";
 
 export const ApiContext=createContext(null)
-const apiUrl=process.env.NODE_ENV==='production' ? 'https://chat-app-bw48oq.fly.dev/api' : 'http://localhost:5000/api'
+const apiUrl=process.env.NODE_ENV==='production' ? 'https://backend-aged-leaf-8417.fly.dev/api' : 'http://localhost:5000/api'
 const ApiProvider=(props)=>{
     return (
         <ApiContext.Provider value={{apiUrl}}>
