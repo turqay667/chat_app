e59a3b1ede147894813b83972c3bd43ca5427746
turgaymammadov <memmedovturqay871@gmail.com>
2025-02-10 14:11:15 +0400

    updated

--- frontend/src/ApiContext.jsx
+++ frontend/src/ApiContext.jsx
@@ -1,7 +1,7 @@
 import { createContext } from "react";
 
 export const ApiContext=createContext(null)
-const apiUrl=process.env.NODE_ENV==='production' ? 'https://chat-app-64fc.onrender.com/api' : 'http://localhost:5000/api'
+const apiUrl=process.env.NODE_ENV==='production' ? 'https://chat-app-bw48oq.fly.dev/api' : 'http://localhost:5000/api'
 const ApiProvider=(props)=>{
     return (
         <ApiContext.Provider value={{apiUrl}}>
