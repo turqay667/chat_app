249842a31f1c854d757185f2be6dd39cd1b69cf0
turgaymammadov <memmedovturqay871@gmail.com>
2025-02-20 00:49:21 +0400

    a

--- frontend/src/ApiContext.jsx
+++ frontend/src/ApiContext.jsx
@@ -1,9 +1,18 @@
-import { createContext } from "react";
+import { createContext, useEffect, useState } from "react";
 export const ApiContext=createContext(null)
 const apiUrl=process.env.NODE_ENV==='production' ? 'https://backend-aged-leaf-8417.fly.dev/api' : 'http://localhost:5000/api'
-const userInfo = JSON.parse(localStorage.getItem('userInfo'))
-const token=userInfo?.data?.token
+
+
+
 const ApiProvider=(props)=>{
+const [userInfo,setUserInfo] = useState(null)
+const [token, setToken]=useState(null)
+
+  useEffect(()=>{
+    const storedUserInfo=JSON.parse(localStorage.getItem('userInfo')) || {}
+  setUserInfo(storedUserInfo)
+  setToken(userInfo?.data?.token || '')
+  },[])
     return (
         <ApiContext.Provider value={{apiUrl, userInfo, token}}>
           {props.children}
