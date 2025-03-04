b3057185dccd9830f10056de6ed57456ee0cb831
turgaymammadov <memmedovturqay871@gmail.com>
2025-03-04 20:34:25 +0400

    updated

--- frontend/src/app/components/Sidebar.tsx
+++ frontend/src/app/components/Sidebar.tsx
@@ -21,15 +21,15 @@ const Sidebar=()=>{
         <div className="logo"><a > <BsChatSquareText fontSize={34}/></a></div>
   <div className="navbar-menu" >
 <ul className="sidebar-menu nav">
-<li><a className="btn" id="chat-tab" data-bs-toggle="tab" role="tab" data-bs-target="#chat"  aria-selected="true"  ><PiChats /></a></li>
-<li><a className="btn"  id="contacts-tab" data-bs-toggle="tab"  role="tab" data-bs-target="#contacts"   aria-selected="false" > <RiContactsLine fontSize={32}/></a></li>
-<li><a className="btn"  id="settings-tab" data-bs-toggle="tab"  role="tab" data-bs-target="#settings"   aria-selected="false" > <CiSettings   /></a></li>
+<li><a title="chats" className="btn" id="chat-tab" data-bs-toggle="tab" role="tab" data-bs-target="#chat"  aria-selected="true"  ><PiChats /></a></li>
+<li><a title="contacts"  className="btn"  id="contacts-tab" data-bs-toggle="tab"  role="tab" data-bs-target="#contacts"   aria-selected="false" > <RiContactsLine fontSize={32}/></a></li>
+<li><a title="settings" className="btn"  id="settings-tab" data-bs-toggle="tab"  role="tab" data-bs-target="#settings"   aria-selected="false" > <CiSettings   /></a></li>
 </ul>
 <ul className="sidebar-menu nav">
 <li><a className="btn">
   {theme==='dark' ? <CiLight  onClick={handleTheme}/> : <CiDark fontSize={32} onClick={handleTheme}/> }
   </a></li>
-<li><a className="btn" ><FiLogOut  onClick={handleLogout}/></a></li>
+<li><a title="logout" className="btn" ><FiLogOut  onClick={handleLogout}/></a></li>
 </ul>
 </div>
 </div>
