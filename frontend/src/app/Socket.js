7bcb8a43a187563bd27e3d97d6ee3fa68abf3e6e
turgaymammadov <memmedovturqay871@gmail.com>
2025-04-11 17:52:12 +0400

    updated

--- frontend/src/app/Socket.js
+++ frontend/src/app/Socket.js
@@ -1,3 +1,6 @@
 import {io} from "socket.io-client";
 const URL=process.env.NODE_ENV==='production' ? 'https://chat-app-64fc.onrender.com' : "http://localhost:5000";
-export const socket=io(URL)
+export const socket=io(URL, {
+    withCredentials:true
+
+})
